ec23ae028e74eb2a4d7a814faf21a83270781def
Aidan Fraser <aaaidan@gmail.com>
2015-09-12 23:03:41 +1200

    Added test for optional whitespace, uncommented all tests

--- html-parser-test.js
+++ html-parser-test.js
@@ -85,64 +85,67 @@ function testThrows(title, executor) {
 
 
 
-// // chr
-// testEqual(
-//     "chr doesn't parse wrong char", function() { 
-//     return chr("b").parse("a"); }, null);
-// testEqual(
-//     "chr parses correct input", function() { 
-//     return chr("a").parse("a"); }, "a");
-// testEqual(
-//     "chr parses only what it's asked", function() { 
-//     return chr("a").parse("abc"); }, "a");
+// chr
+testEqual(
+    "chr doesn't parse wrong char", function() { 
+    return chr("b").parse("a"); }, null);
+testEqual(
+    "chr parses correct input", function() { 
+    return chr("a").parse("a"); }, "a");
+testEqual(
+    "chr parses only what it's asked", function() { 
+    return chr("a").parse("abc"); }, "a");
 
-// // digit
-// testEqual(
-//     "digit doesn't parse wrong char", function() { 
-//     return digit.parse("a"); }, null);
-// testEqual(
-//     "digit parses correct input", function() { 
-//     return digit.parse("5"); }, "5");
-// testEqual(
-//     "digit parses only what it's asked", function() { 
-//     return digit.parse("5273"); }, "5");
+// digit
+testEqual(
+    "digit doesn't parse wrong char", function() { 
+    return digit.parse("a"); }, null);
+testEqual(
+    "digit parses correct input", function() { 
+    return digit.parse("5"); }, "5");
+testEqual(
+    "digit parses only what it's asked", function() { 
+    return digit.parse("5273"); }, "5");
 
-// // seq
-// testEqual(
-//     "seq works with chr", function() { 
-//     return seq(chr("a"), chr("b")).parse("ab"); }, "ab");
-// testEqual(
-//     "seq parses only what it's asked with chr", function() { 
-//     return seq(chr("a"), chr("b")).parse("abc"); }, "ab");
-// testEqual(
-//     "seq parses only what it's asked with digit", function() { 
-//     return seq(digit, digit, digit).parse("6460"); }, "646");
-// testEqual(
-//     "seq returns nothing when one of its elements fails", function() { 
-//     return seq(digit, digit, digit).parse("6a60"); }, null);
+// seq
+testEqual(
+    "seq works with chr", function() { 
+    return seq(chr("a"), chr("b")).parse("ab"); }, "ab");
+testEqual(
+    "seq parses only what it's asked with chr", function() { 
+    return seq(chr("a"), chr("b")).parse("abc"); }, "ab");
+testEqual(
+    "seq parses only what it's asked with digit", function() { 
+    return seq(digit, digit, digit).parse("6460"); }, "646");
+testEqual(
+    "seq returns nothing when one of its elements fails", function() { 
+    return seq(digit, digit, digit).parse("6a60"); }, null);
 
-// // word
-// testEqual(
-//     "word only parses correct input", function() { 
-//     return word("abc").parse("abc"); }, "abc");
-// testEqual(
-//     "word only parses what it's asked", function() { 
-//     return word("abc").parse("abcd"); }, "abc");
-// testEqual(
-//     "word fails correctly", function() { 
-//     return word("abc").parse("abdc"); }, null);
+// word
+testEqual(
+    "word only parses correct input", function() { 
+    return word("abc").parse("abc"); }, "abc");
+testEqual(
+    "word only parses what it's asked", function() { 
+    return word("abc").parse("abcd"); }, "abc");
+testEqual(
+    "word fails correctly", function() { 
+    return word("abc").parse("abdc"); }, null);
+testEqual(
+    "word can be part of another parser", function() { 
+    return seq( word("abc"), _, word("def") ).parse("abc   def"); }, "abc   def");
 
-// // wsChar
-// testEqual("wsChar fails correctly", function() {
-// 	return wsChar.parse("ABC") }, null);
-// testEqual("wsChar parses single space", function() {
-// 	return wsChar.parse(" ") }, " ");
-// testEqual("wsChar parses single tab", function() {
-// 	return wsChar.parse("\t") }, "\t");
-// testEqual("wsChar parses single LF", function() {
-// 	return wsChar.parse("\n") }, "\n");
-// testEqual("wsChar parses single CR", function() {
-// 	return wsChar.parse("\r") }, "\r");
+// wsChar
+testEqual("wsChar fails correctly", function() {
+	return wsChar.parse("ABC") }, null);
+testEqual("wsChar parses single space", function() {
+	return wsChar.parse(" ") }, " ");
+testEqual("wsChar parses single tab", function() {
+	return wsChar.parse("\t") }, "\t");
+testEqual("wsChar parses single LF", function() {
+	return wsChar.parse("\n") }, "\n");
+testEqual("wsChar parses single CR", function() {
+	return wsChar.parse("\r") }, "\r");
 
 // many
 testEqual("many fails correctly", function() {
@@ -185,6 +188,28 @@ testEqual("_ parses LFCR", function() {
 	return _.parse("\n\r") }, "\n\r");
 testEqual("_ parses only what asked ", function() {
 	return _.parse("   \r\n   \t  ABC") }, "   \r\n   \t  ");
+
+// optional whitespace
+testEqual("__ matches optionally", function() {
+	return __.parse("ABC") }, "");
+testEqual("__ parses single space", function() {
+	return __.parse(" ") }, " ");
+testEqual("__ parses 4 spaces", function() {
+	return __.parse("    ") }, "    ");
+testEqual("__ parses single LF", function() {
+	return __.parse("\n") }, "\n");
+testEqual("__ parses 4x LF", function() {
+	return __.parse("\n\n\n\n") }, "\n\n\n\n");
+testEqual("__ parses single CR", function() {
+	return __.parse("\r") }, "\r");
+testEqual("__ parses 4x CR", function() {
+	return __.parse("\r\r\r\r") }, "\r\r\r\r");
+testEqual("__ parses CRLF", function() {
+	return __.parse("\r\n") }, "\r\n");
+testEqual("__ parses LFCR", function() {
+	return __.parse("\n\r") }, "\n\r");
+testEqual("__ parses only what asked ", function() {
+	return __.parse("   \r\n   \t  ABC") }, "   \r\n   \t  ");
 
 
 console.log("Tests Passed:", testsPassed);
